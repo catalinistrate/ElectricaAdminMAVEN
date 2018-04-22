875b2612c60bb6c1806e860bc815b727a4e7c10f
Istrate Catalin-Flaviu <cistrate13@gmail.com>
2018-04-22 19:35:29 +0300

    rest of WBT

--- src/test/java/controller/ClientControllerTest.java
+++ src/test/java/controller/ClientControllerTest.java
@@ -132,5 +132,49 @@ public class ClientControllerTest {
         assert(result.equals("Year can't be 0 or less!"));
     }
 
+    @Test
+    public void testAddClientIndex_whenToPayIsNegative_expectedMessage(){
+        ClientController clientController = new ClientController();
+        String result = clientController.AddClientIndex(new Client("asd","asd","asd"),2018,9,-10);
+
+        assert(result.equals("Money to pay can't be less than 0!"));
+    }
+
+    @Test
+    public void testAddClientIndex_whenClientIsInvalid_expectedMessage(){
+        ClientController clientController = new ClientController();
+        String result = clientController.AddClientIndex(new Client("asd","asd","asd"),2018,9,10);
+        assert(result.equals("Address does not contain number."));
+    }
+
+    @Test
+    public void testAddClientIndex_whenClientIsNotExisting_expectedMessage(){
+        ClientController clientController = new ClientController();
+        String result = clientController.AddClientIndex(new Client("catalin istrate",
+                "some number some street","5"),2018,9,10);
+        assert(result.equals("Client does not exist!"));
+    }
+
+    @Test
+    public void testAddClientIndex_whenInputIsFine_expectedNull(){
+        ClientController clientController = new ClientController();
+        clientController.AddClient("catalin istrate",
+                "some number some street","5");
+        String result = clientController.AddClientIndex(new Client("catalin istrate",
+                "some number some street","5"),2018,9,10);
+        assert(result == null);
+    }
+
+    @Test
+    public void testAddClientIndex_whenIndexAlreadyExists_expectedMessage(){
+        ClientController clientController = new ClientController();
+        clientController.AddClient("catalinn istrate",
+                "some number some street","51");
+        clientController.AddClientIndex(new Client("catalinn istrate",
+                "some number some street","51"),2018,9,10);
+        String result = clientController.AddClientIndex(new Client("catalinn istrate",
+                "some number some street","51"),2018,9,10);
+        assert(result.equals("Monthly index already exists!"));
+    }
 
 }
